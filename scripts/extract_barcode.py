48abf31271823c7026b02fb3509cbe1775d0202b
John Beaulaurier <john.beaulaurier@nanoporetech.com>
2022-07-29 14:56:44 -0700

    multiome bug fix in adapter extraction

--- scripts/extract_barcode.py
+++ scripts/extract_barcode.py
@@ -52,8 +52,10 @@ def parse_args():
     parser.add_argument(
         "-k",
         "--kit",
-        help="Specify either the 10X 3' (3prime) or 5' (5prime) gene expression \
-        kit [3prime]",
+        help="Specify either the 10X 3' gene expression kit (3prime), the 5' \
+        gene expression kit (5prime), or the multiome kit (multiome) This \
+        determines which adapter sequences to search for in the reads \
+        [3prime]",
         default="3prime",
         type=str,
     )
@@ -169,10 +171,13 @@ def parse_args():
     args = parser.parse_args()
 
     # verify kit selection
-    if (args.kit != "3prime") and (args.kit != "5prime"):
-        raise Exception("Invalid kit name! Specify either 3prime or 5prime.")
+    if (args.kit != "3prime") and (args.kit != "5prime") and (args.kit != "multiome"):
+        raise Exception(
+            "Invalid kit name! Specify either 3prime, 5prime or \
+        multiome."
+        )
 
-    if args.kit == "3prime":
+    if (args.kit == "3prime") or (args.kit == "multiome"):
         # Read1 adapter
         args.adapter1_seq = "CTACACGACGCTCTTCCGATCT"
     elif args.kit == "5prime":
@@ -399,7 +404,7 @@ def align_adapter(tup):
     # Use only the specified suffix length of adapter1
     adapter1_probe_seq = args.adapter1_seq[-args.adapter1_suff_length :]
 
-    if args.kit == "3prime":
+    if (args.kit == "3prime") or (args.kit == "multiome"):
         # Compile the actual probe sequence of <adapter1_suffix>NNN...NNN<TTTTT....>
         probe_seq = "{a1}{bc}{umi}{pT}".format(
             a1=adapter1_probe_seq,
